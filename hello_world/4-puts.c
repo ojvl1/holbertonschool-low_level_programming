efc25da45edd3f8f23bc82691861864f33b3b03b
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-10-15 23:22:31 -0400

    program that print

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - pirnting words
+ * main - printing words
  *
  * Return:devolver el 0
  *
@@ -8,19 +8,7 @@
 int main(void)
 {
 	puts("\"{rogramming is like
-buliding a multilingual puzzle");
-	return (0);
-}#include <stdio.h>
-/**
- * main - pirnting words
- *
- * Return:devolver el 0
- *
- */
-int main(void)
-{
-	puts("\"{rogramming is like
-buliding a multilingual puzzle");
+building a multilingual puzzle");
 	return (0);
 }#include <stdio.h>
 /**
