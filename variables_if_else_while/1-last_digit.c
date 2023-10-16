88672dd6bcec35b140734e0f4fbc28526162f31d
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-10-16 14:17:18 -0400

    ramdom number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else if (r == 0)
 	{
-		printf("Last digit of %d is %d and is  0\n", n, r);
+		printf("Last digit of %d is %d and is 0\n", n, r);
 	}
 	else if (r < 6 && n != 0)
 	{
