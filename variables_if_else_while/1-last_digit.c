1bb075ec2724409aad375a25b73a4fee3e9bd5bc
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-10-16 14:12:48 -0400

    ramdom number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,15 +20,15 @@ int main(void)
 
 	if (r > 5)
 	{
-		printf("Last digit of %d is %d  grater than 5\n", n, r);
+		printf("Last digit of %d is %d and is grater than 5\n", n, r);
 	}
 	else if (r == 0)
 	{
-		printf("Last digit of %d is %d 0\n", n, r);
+		printf("Last digit of %d and is %d  0\n", n, r);
 	}
 	else if (r < 6 && n != 0)
 	{
-		printf("Last digit of %d is %d less than 6 and not 0\n", n, r);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, r);
 	}
 	return (0);
 }
