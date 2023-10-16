208fc1ff99b0082b94f83d59f98feb66425acae9
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-10-15 23:19:08 -0400

    program that print

---
+++ hello_world/4-print.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - pirnting words
+ *
+ * Return:devolver el 0
+ *
+ */
+int main(void)
+{
+	puts("\"{rogramming is like
+buliding a multilingual puzzle");
+	return (0);
+}#include <stdio.h>
+/**
+ * main - pirnting words
+ *
+ * Return:devolver el 0
+ *
+ */
+int main(void)
+{
+	puts("\"{rogramming is like
+buliding a multilingual puzzle");
+	return (0);
+}#include <stdio.h>
+/**
+ * main - pirnting words
+ *
+ * Return:devolver el 0
+ *
+ */
+int main(void)
+{
+	puts("\"{rogramming is like
+buliding a multilingual puzzle");
+	return (0);
+}
