8e8f2cb1ff8e99e98bce0e88a7ee5fb38f2c7092
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-10-16 10:59:59 -0400

    assign a random number to the variable n

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#icludo <stdlib.h>
+#includo <time.h>
+/*more headers goes there */
+
+/*betty style doc for function main goes there */
+int main(void)
+{ 
+	int n;
+
+	srand(time(0));
+	n= rand() - RAND_MAX / 2;
+	
+	if(n > 0\n);
+        printf(is positive);
+	
+	if(n == 0\n);
+	printf(is zero);
+
+	if(n < 0\n);
+	printf(is negative);
+
+	return(0\n);
+
+}	
