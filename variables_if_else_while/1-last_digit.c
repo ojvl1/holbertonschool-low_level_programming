8758a5b9670ed39735168c821d0e0a12333f1cc8
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-10-16 14:09:07 -0400

    ramdom number

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0
+ * more headers goes there
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int r;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	r = n % 10;
+
+	if (r > 5)
+	{
+		printf("Last digit of %d is %d  grater than 5\n", n, r);
+	}
+	else if (r == 0)
+	{
+		printf("Last digit of %d is %d 0\n", n, r);
+	}
+	else if (r < 6 && n != 0)
+	{
+		printf("Last digit of %d is %d less than 6 and not 0\n", n, r);
+	}
+	return (0);
+}
