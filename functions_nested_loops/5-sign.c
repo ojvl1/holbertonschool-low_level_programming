c2536bc702ef9b4dd19404996f1c5057ed53effc
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-10-19 15:48:39 -0400

    print the sign of a number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,20 +9,20 @@ int print_sign(int n)
 	if (n > 0)
 	{
 	_putchar('+');
+	n = 1;
 	}
-	return (1);
 
 	if (n == 0)
 	{
 	_putchar('0');
+	n = 0;
 	}
-	return (0);
 
 	if (n < 0)
 	{
 	_putchar('-');
+	n = -1;
 	}
-	return (-1);
 
-_putchar('\n');
+	return (n);
 }
