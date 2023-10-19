bc405f0350d2c66b0b2f87147c7f696a776e8573
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-10-19 10:29:13 -0400

    print the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ *print_alphabet - print the alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+_putchar('\n');
+
+}
