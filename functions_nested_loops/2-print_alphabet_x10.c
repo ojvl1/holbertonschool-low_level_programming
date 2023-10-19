56dc5aa82a16ab03643559ead6442b8cfda13502
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-10-19 12:04:26 -0400

    print the alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - entry point
+ */ 
+void print_alphabet_x10(void)
+{
+	int alpha;
+	char bet;
+
+	for (alpha = '0'; alpha <= '9'; alpha++)
+	{
+		for (bet = 'a'; bet <= 'z'; bet++)
+		{
+		_putchar(alpha);
+		}
+
+		_putchar('\n');
+
+	}
+
+}
