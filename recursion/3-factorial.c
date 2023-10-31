8fad5736920a27aa2dba6835102637cba40ce500
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-10-31 12:59:11 -0400

    Write a function that returns the factorial of a given number

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,7 +2,7 @@
 /**
  *factorial - function that return the factorial of a given number
  *@n: pointer
- *Return: -1 if an error
+ *Return: -1 if an error, 1, 0
  */
 int factorial(int n)
 {
@@ -11,8 +11,12 @@ int factorial(int n)
 		return (-1);
 	}
 
-	else if (n == 0 || n == 1)
+	else if (n == 1)
 	{
 		return (1);
 	}
+
+	else
+
+		return (n * factorial(n - 1));
 }
