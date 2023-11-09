337eeec637a5c9b9818a73dfe1a2d395783a9a19
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-11-09 13:38:09 -0400

    program that print the name of the file

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *@main - Print file name that it was compiled on
+ *main - Print file name that ot was compiled on
  *Return: 0
  */
 int main(void)
 {
-	printf ("%s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
