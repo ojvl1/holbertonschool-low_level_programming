de4a713a6e70407f4e1042d4c631c42d5ceadd55
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-11-11 20:36:34 -0400

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-print_dog.c -o c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *print_dog - function that prints structure
+ *
+ *@d: dog variable
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	printf("Age: %f\n", d->age);
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+}
