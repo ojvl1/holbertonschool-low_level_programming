009e614b8123a991a2ed1a715fd47139e4c81a87
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-11-13 22:47:15 -0400

    This file should contain all the function prototypes and data structures used by the program

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,16 +1,15 @@
 #ifndef CALC_H
 #define CALC_H
 /**
- *struct op - Struct op
+ * struct op - Struct op
  *
- *@op: The operator
- *@f: the function
+ * @op: The operator
+ * @f: The function associated
  */
-
 typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
