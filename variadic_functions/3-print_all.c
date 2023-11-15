f723f4f68635aaf81ff33e1efc136595fd1afac5
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-11-15 16:28:00 -0400

    Write a function that prints anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -32,8 +32,7 @@ void print_all(const char * const format, ...)
 				case 's':
 				str = va_arg(args, char *);
 				if (str == NULL)
-					printf("(nil)");
-				else
+					str ="(nil)";
 				printf("%s", str);
 					break;
 				default:
