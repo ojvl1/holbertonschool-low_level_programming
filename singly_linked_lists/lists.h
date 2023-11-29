a57f055f19e196dfcb863a2fe14df4376034493f
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-11-29 13:07:32 -0400

    Function Prototypes

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
-
-/* Function Prototypes */
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,9 +11,9 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
