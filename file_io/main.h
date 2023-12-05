72609ead935095646769f40a450cbc37d1baec34
Osvaldo Antompietri <97262675+ojvl1@users.noreply.github.com>
2023-12-04 20:47:58 -0400

    Delete file_io directory

--- file_io/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * Prototypes
- */
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char
-int append_text_to_file(const char *filename, char *text_content);
-#endif
