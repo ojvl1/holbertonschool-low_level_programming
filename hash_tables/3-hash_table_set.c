6c4a7ac9966ea6f376ec09cc880e55f582491b2c
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-12-06 16:06:30 -0400

    function that adds an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -22,12 +22,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	tmp = ht->array[idx];
 	while (tmp != NULL)
+	{
 		if (strcmp(tmp->key, key) == 0)
 		{
 			tmp->value = strdup(value);
 			return (1);
-			tmp = tmp->next;
 		}
+		tmp = tmp->next;
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
